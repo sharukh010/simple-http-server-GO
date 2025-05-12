8f017b0c9fc1b840e61afea8c5c7101d6b6c2147
= <sharukhprogramming@gmail.com>
2025-05-12 21:28:14 +0530

    added hellohandler and its route

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter,r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w,"404 not found",http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w,"method is not supported",http.StatusNotFound)
+		return 
+	}
+	fmt.Fprintf(w,"hello! Sharukh Khan")
+}
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
+	http.HandleFunc("/hello",helloHandler)
 	
 	fmt.Printf("Starting server at port 8081\n")
 	if err := http.ListenAndServe(":8081",nil);err != nil{
