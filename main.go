300bb88fd7e3321c6c63ddd0c719df73fb3d4819
= <sharukhprogramming@gmail.com>
2025-05-12 21:46:04 +0530

    added form handler and route

--- main.go
+++ main.go
@@ -6,6 +6,17 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter,r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w,"ParseForm() err: %s",err)
+		return 
+	}
+	fmt.Fprintf(w,"POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w,"Name = %s\n",name)
+	fmt.Fprintf(w,"Address = %s\n",address)
+}
 func helloHandler(w http.ResponseWriter,r *http.Request){
 	if r.URL.Path != "/hello"{
 		http.Error(w,"404 not found",http.StatusNotFound)
@@ -21,6 +32,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/",fileServer)
 	http.HandleFunc("/hello",helloHandler)
+	http.HandleFunc("/form",formHandler)
 	
 	fmt.Printf("Starting server at port 8081\n")
 	if err := http.ListenAndServe(":8081",nil);err != nil{
